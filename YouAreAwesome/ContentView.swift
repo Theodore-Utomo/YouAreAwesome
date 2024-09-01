9bc4387bd94ae1dde59fa24f06f10e83ed24d444
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-01 18:37:53 -0400

    Button Challenges

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
     var body: some View {
         VStack {
             Image(systemName: "swift")
@@ -16,11 +17,25 @@ struct ContentView: View {
                 .foregroundStyle(.mint)
                 .padding(.horizontal, 30.0)
                 .padding(.bottom, 100.0)
-            Text("You Are Awesome!")
+            Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.indigo)
                 .italic()
+                .padding()
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action preformed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
         }
     }
 }
