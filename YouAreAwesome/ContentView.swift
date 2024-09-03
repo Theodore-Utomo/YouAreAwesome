621f9c78e3adcd84c44e6ded557671f7428c723d
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-03 17:15:04 -0400

    Added Image

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,42 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = "Namaste"
     var body: some View {
-        VStack {
-            
-        }
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.mint)
-                .padding(.horizontal, 30.0)
-                .padding(.bottom, 100.0)
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.indigo)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
-                .padding()
-            HStack {
-                Button("Awesome") {
-                    // This is the action preformed when the button is pressed
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
+        
+        //            Rectangle()
+        //                .fill(
+        //                    Gradient(colors: [.maroonBC, .skyBlue])
+        //                )
+        //                .ignoresSafeArea()
+        
+        //            Rectangle()
+        //                .fill(
+        //                    LinearGradient(
+        //                        colors: [.red, .indigo],
+        //                        startPoint: .topLeading,
+        //                        endPoint: .bottomTrailing
+        //                    )
+        //                )
+        
+        //            Rectangle()
+        //                .fill(
+        //                    RadialGradient(
+        //                        colors: [.red, .white, .blue],
+        //                        center: .center,
+        //                        startRadius: 50,
+        //                        endRadius: 100
+        //                    )
+        //                )
+        
+        //            Rectangle()
+        //                .fill(
+        //                    AngularGradient(
+        //                        colors: [.red, .orange, .yellow, .green, .blue, .indigo, .purple],
+        //                        center: .center
+        //                    )
+        //                )
+        
+        
+        GeometryReader {geometry in
+            VStack {
+                //                Text("You have Skills!")
+                //                    .font(.largeTitle)
+                //                    .fontWeight(.black)
+                //                    .foregroundStyle(Color("Gold-BC"))
+                //                    .padding()
+                //                    .background(Color("Maroon-BC"))
+                //                    .cornerRadius(15)
                 
-                Button("Great") {
-                    messageString = "You Are Great!"
+                Spacer()
+                
+                Image(systemName: "speaker.wave.3", variableValue: 0.68)
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.multicolor)
+                    .padding()
+                    .background(Color(hue: 0.502, saturation: 0.289, brightness: 0.966))
+                    .cornerRadius(30)
+                    .shadow(color: .gray, radius: 8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.teal, lineWidth: 2)
+                    )
+                    .padding()
+                                   
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.pink)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding(.bottom)
+                
+                Spacer()
+                
+                Rectangle()
+                    .fill(.maroonBC)
+                    .frame(width: geometry.size.width * (2/3), height: 1)
+                
+                HStack {
+                    Button("Awesome") {
+                        // This is the action preformed when the button is pressed
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
+                .padding()
+                .foregroundStyle(Color("Gold-BC"))
+                .fontWeight(.heavy)
+                .tint(Color("Maroon-BC"))
             }
-            .border(.purple, width: 3)
         }
+        
     }
 }
 
